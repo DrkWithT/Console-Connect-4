bfa96d7ab9388c30850829c28447dd05137e2494
DerkTee <derektan3510@gmail.com>
2022-10-21 21:08:19 -0700

    Added some check func comments.

--- headers/board.h
+++ headers/board.h
@@ -43,6 +43,14 @@ int Board_getRowOfMove(const Board *self);
 
 int Board_getColOfMove(const Board *self);
 
+/**
+ * @brief Inserts a piece at the specified column by index. The piece is then translated downward to the last, lowest blank cell.
+ * 
+ * @param self 
+ * @param col_idx 
+ * @param c 
+ * @return _Bool 
+ */
 _Bool Board_putPiece(Board *self, int col_idx, char c);
 
 void Board_clearCells(Board *self);
@@ -51,7 +59,19 @@ _Bool static Board_checkVerticalsAt(Board *self, int row_idx, int col_idx);
 
 _Bool static Board_checkHorizontalsAt(Board *self, int row_idx, int col_idx);
 
-_Bool static Board_checkDiagonalsAt(Board *self, int row_idx, int col_idx);
+_Bool static Board_checkUpperDiagonalsAt(Board *self, int row_idx, int col_idx);
+
+/**
+ * @brief This is a generalized function that checks for any 4-streak of a player piece along a diagonal. The diagonal is specified with step components, like 2D vector x-component or y-component.
+ * 
+ * @param self The Board ptr.
+ * @param row_idx Position row.
+ * @param col_idx Position column.
+ * @param row_step Step magnitude to next row from position.
+ * @param col_step Step magnitude to next col from position.
+ * @return _Bool If the diagonal has a win.
+ */
+_Bool static Board_checkDiagonalFrom(Board *self, int row_idx, int col_idx, int row_step, int col_step);
 
 _Bool Board_hasWinner(Board *self, int row_idx, int col_idx);
 
