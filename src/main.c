df63360b4fb8fe6bb56cb4c23c6055ea78f7e8e2
DerkTee <derektan3510@gmail.com>
2022-10-22 22:02:58 -0700

    Wrote initial game loop code, help funcs.

--- src/main.c
+++ src/main.c
@@ -17,11 +17,17 @@
  * oooOOox
  */
 
+/// User input buffer size:
+#define INPUT_BUF_SIZE 4
+
 static Board *Game_Board = NULL;
 static Renderer *Game_Drawer;
+static char Input_Buffer[INPUT_BUF_SIZE];
 
 _Bool Game_Initialize()
 {
+  memset(Input_Buffer, '\0', INPUT_BUF_SIZE);
+
   Game_Board = Board_Constr(DEFAULT_BOARD_ROWS, DEFAULT_BOARD_COLS);
 
   if (!Game_Board)
@@ -60,13 +66,59 @@ _Bool Game_Intro()
   return c == 'y';
 }
 
+void Game_Prompt_User(int player_num, char *input_buf, int buf_size)
+{
+  // show prompt
+  printf("P%i, enter your column: ", player_num);
+
+  // get input with finite length and null terminator
+  fgets(input_buf, 1, stdin);
+  input_buf[buf_size - 1] = '\0';
+}
+
+int Game_Atoi(const char *input_buf)
+{
+  int temp = atoi(input_buf);
+
+  if (input_buf[0] == '0')
+    temp = 0;
+  else if (temp == 0)
+    temp = -1; // default conversion for NaN literals!
+  
+  return temp;
+}
+
+inline _Bool Game_Validate_Input(int col_num)
+{
+  return col_num >= 0 && col_num < Game_Board->cols;
+}
+
+void Game_Say_Winner(char player_piece)
+{
+  switch (player_piece)
+  {
+  case PLAYER_1_PIECE:
+    puts("Player 1 won.");
+    break;
+  case PLAYER_2_PIECE:
+    puts("Player 2 won.");
+    break;
+  case BLANK_CELL:
+  default:
+    puts("Tie!");
+    break;
+  }
+}
+
 int main()
 {
   // Initialize game variables.
-  _Bool turn_is_player1 = true;
+  int player_number = 1;
   char game_piece = PLAYER_1_PIECE;
   char game_winner = BLANK_CELL;
   int chosen_column = 0;
+  int move_row = 0;
+  int move_col = 0;
 
   // Initialize special objects and setup exit handler in case.
   if (!Game_Initialize())
@@ -86,10 +138,46 @@ int main()
   // Run game loop.
   while (1)
   {
-    Renderer_drawAll(Game_Drawer);
-    break; // todo: replace with full-board and winner checks for breaking the loop!
+    Renderer_drawAll(Game_Drawer); // draw board
+
+    Game_Prompt_User(player_number, Input_Buffer, INPUT_BUF_SIZE);
+
+    chosen_column = Game_Atoi(Input_Buffer); // convert input string to column index or -1
+
+    if (Game_Validate_Input(chosen_column))
+    {
+      Board_putPiece(Game_Board, chosen_column, game_piece);
+      move_row = Board_getRowOfMove(Game_Board);
+      move_col = Board_getColOfMove(Game_Board);
+    }
+    else
+    {
+      game_winner = BLANK_CELL;
+      break; // treat invalid cols as quitting!
+    }
+
+    if (Board_hasWinner(Game_Board, move_row, move_col))
+    {
+      game_winner = game_piece;
+      break;
+    }
+    else
+      game_winner = BLANK_CELL;
+
+    // Toggle current player and piece.
+    if (player_number == 1)
+    {
+      player_number = 2;
+      game_piece = PLAYER_2_PIECE;
+    }
+    else
+    {
+      player_number = 1;
+      game_piece = PLAYER_1_PIECE;
+    }
   }
 
-  // todo: Print winner!
+  Game_Say_Winner(game_winner);
+
   return 0;
 }
